85019ca591aee969e33ac6511099a51b7b38c19e
haben0 <80583582+haben0@users.noreply.github.com>
2021-06-26 11:47:06 +0100

    update

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+
+var B =("Hello World")
+window.alert(B);
+
+document.write("Hello World");
+
+var A = "This is a string";
+document.write(A);
+
+
+document.write("Lisa told Bart, \"Knock it off, Bart! or I'll tell dad!\"");
+
+document.write("\"Be who you are and say what you feel,"
++ " beacause those who mind don\'t matter abd those who matter don\'t mind.\""
++ "-Dr.Seuss");
+
+var Family = "The Arezzinis", Dad = "Jermiah", Mon = "Hermoine",
+    Daughter = "Penny", Son = "Zorro";
+document.write(Dad);
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues);
+
+document.write(3 + 3);
