b1a6001e6c84dcc0a8c8b01338c5d2517bcfdf3c
haben0 <80583582+haben0@users.noreply.github.com>
2021-06-26 12:00:20 +0100

    update

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -2,24 +2,37 @@
 var B =("Hello World")
 window.alert(B);
 
+document.write("<br>")
 document.write("Hello World");
+var withBreaks = "Hello World. \n My name is Jennifer. \n What is your name?"
 
+document.write("<br>")
+document.write("<br>")
 var A = "This is a string";
 document.write(A);
 
-
+document.write("<br>")
+document.write("<br>")
 document.write("Lisa told Bart, \"Knock it off, Bart! or I'll tell dad!\"");
 
+document.write("<br>")
+document.write("<br>")
 document.write("\"Be who you are and say what you feel,"
-+ " beacause those who mind don\'t matter abd those who matter don\'t mind.\""
++ " beacause those who mind don\'t matter and those who matter don\'t mind.\""
 + "-Dr.Seuss");
 
+document.write("<br>")
+document.write("<br>")
 var Family = "The Arezzinis", Dad = "Jermiah", Mon = "Hermoine",
     Daughter = "Penny", Son = "Zorro";
 document.write(Dad);
 
+document.write("<br>")
+document.write("<br>")
 var blues = "I have the blues.";
 var blues = blues.fontcolor("blue");
 document.write(blues);
 
+document.write("<br>")
+document.write("<br>")
 document.write(3 + 3);
